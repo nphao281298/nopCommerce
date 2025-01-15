0382330e207b6e040e4aeb01aaf8d98876fd3b19
Hao Nguyen <nphao281298>
2025-01-15 23:26:50 +0700

    Level_13-15012025

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,10 +1,13 @@
 package commons;
 
+import com.google.common.base.VerifyException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -54,6 +57,40 @@ public class BaseTest {
         driver.get(url);
         driver.manage().window().maximize();
         return driver;
+    }
+
+    protected boolean verifyTrue(boolean condition){
+        boolean pass = true;
+        try {
+            Assert.assertTrue(condition);
+        } catch (Throwable e) {
+            pass = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+    protected boolean verifyFalse(boolean condition){
+        boolean pass = true;
+        try {
+            Assert.assertFalse(condition);
+        } catch (Throwable e) {
+            pass = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
+    }
+    protected boolean verifyEquals(Object actual, Object expected){
+        boolean pass = true;
+        try {
+            Assert.assertEquals(actual, expected);
+        } catch (Throwable e) {
+            pass = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+        }
+        return pass;
     }
 
     public String getEmailRandom() {
