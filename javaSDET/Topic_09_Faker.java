b62542ee729088c316ca4fd4972218222b306a35
Hao Nguyen <nphao281298>
2025-03-04 23:23:29 +0700

    Level_29_Data_Excel

--- javaSDET/Topic_09_Faker.java
+++ javaSDET/Topic_09_Faker.java
@@ -1,12 +1,18 @@
 import com.github.javafaker.Faker;
+import utilities.ExcelConfig;
 
 import java.util.Locale;
 
 public class Topic_09_Faker {
     public static void main(String[] args) {
-        Faker faker = new Faker(new Locale("vi"));
+//        Faker faker = new Faker(new Locale("vi"));
+//
+//        System.out.println(faker.leagueOfLegends().rank());
+//        System.out.println(faker.address().city());
 
-        System.out.println(faker.leagueOfLegends().rank());
-        System.out.println(faker.address().city());
+        ExcelConfig excelConfig = ExcelConfig.getExcelData();
+        excelConfig.switchToSheet("userData");
+        System.out.println(excelConfig.getCellData("FirstName",1));
+
     }
 }
